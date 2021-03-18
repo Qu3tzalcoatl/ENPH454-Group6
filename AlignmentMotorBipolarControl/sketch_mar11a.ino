5e44fe6d13286257256bf519553c63bbc34d5da2
Ryan Burley <16rjb7@queensu.ca>
2021-03-18 10:57:15 -0400

    Update sketch_mar11a.ino

--- AlignmentMotorBipolarControl/sketch_mar11a.ino
+++ AlignmentMotorBipolarControl/sketch_mar11a.ino
@@ -7,6 +7,9 @@ void setup()
   const int dirPin = 22;
   const int stepPin = 24;
   const int stepsPerRevolution = 20000;
+
+  const int dir_alignPin = 26;
+  const int step_alignPin = 28;
   // Declare pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
@@ -15,7 +18,11 @@ void setup()
 
   switch (condition) {
   case 1:
-    digitalWrite(dirPin, HIGH); // clockwise rotation
+    digitalWrite(dir_alignPin, LOW); // clockwise rotation
+    turn(step_alignPin, 60);
+    turn(stepPin, 360);
+    digitalWrite(dir_alignPin, HIGH); // counterclockwise rotation
+    turn(step_alignPin, 60);
     break;
   case 2:
     break;
@@ -28,7 +35,15 @@ void setup()
 
 }
 
-for(int x = 0; x < stepsPerRevolution; x++)
+}
+
+void turn(int pin, int angle) 
+{
+  int stepsPerRevolution = 20000;
+  int  stepPin = pin;
+  int iterations = floor(angle*stepsPerRevolution/360);
+
+for(int x = 0; x < iterations; x++)
   {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(1000);
@@ -36,19 +51,3 @@ for(int x = 0; x < stepsPerRevolution; x++)
     delayMicroseconds(1000);
   }
 }
-//void loop()
-//{
-
-  // Spin motor slowly
-  //for(int x = 0; x < stepsPerRevolution; x++)
-  //{
-  //  digitalWrite(stepPin, HIGH);
-  //  delayMicroseconds(2000);
-  //  digitalWrite(stepPin, LOW);
-  //  delayMicroseconds(2000);
-  //}
-  //delay(1000); // Wait a second
-
-  // Spin motor quickly
-  //delayMicroseconds(1000); // Wait a second
-//}
