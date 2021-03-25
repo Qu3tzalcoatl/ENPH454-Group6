58a4c841012af7c541b2cc3b8656e911a0ba65a7
Quetzalcoatl <spencer.c.lee@gmail.com>
2021-03-25 12:03:23 -0400

    Update sketch_mar23.ino

--- AlignmentMotorBipolarControl/sketch_mar23.ino
+++ AlignmentMotorBipolarControl/sketch_mar23.ino
@@ -4,12 +4,14 @@ void setup()
 {
 
   // Define pin connections & motor's steps per revolution
+  const int stepsPerRevolution = 20000;
+  
   const int dirPin = 22;
   const int stepPin = 24;
-  const int stepsPerRevolution = 20000;
-
+  
   const int dir_alignPin = 26;
   const int step_alignPin = 28;
+  
   // Declare pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
@@ -20,47 +22,85 @@ void setup()
 
   switch (condition) {
   case 1:
-    digitalWrite(dir_alignPin, LOW); // clockwise rotation
-    for(int x = 0; x < 5000; x++)
+    digitalWrite(dir_alignPin, HIGH); // clockwise rotation
+    //digitalWrite(step_alignPin, LOW);
+    for(int x = 0; x < 3500; x++)
       {
        digitalWrite(step_alignPin, HIGH);
-       delayMicroseconds(1000);
+       delayMicroseconds(700);
        digitalWrite(step_alignPin, LOW);
-       delayMicroseconds(1000);
+       delayMicroseconds(700);
        }
-    //turn(step_alignPin, 60);
+    //digitalWrite(dir_alignPin, LOW); // counterclockwise rotation
+
     
-    digitalWrite(dirPin, LOW); //conveyor direction
-    //turn(stepPin,360);
-    for(int x = 0; x < 10000; x++)
+    digitalWrite(dirPin, HIGH); //conveyor direction
+    //digitalWrite(stepPin, LOW);
+    for(int x = 0; x < 20000; x++)
       {
        digitalWrite(stepPin, HIGH);
-       delayMicroseconds(1000);
+       delayMicroseconds(400);
        digitalWrite(stepPin, LOW);
-       delayMicroseconds(1000);
+       delayMicroseconds(400);
        }
-
+    //digitalWrite(dirPin, LOW); //conveyor direction
 
     
-    digitalWrite(dir_alignPin, HIGH); // counterclockwise rotation
-    for(int x = 0; x < 5000; x++)
+    digitalWrite(dir_alignPin, LOW); // counterclockwise rotation
+    //digitalWrite(step_alignPin, LOW);
+    for(int x = 0; x < 3500; x++)
       {
        digitalWrite(step_alignPin, HIGH);
-       delayMicroseconds(1000);
+       delayMicroseconds(700);
        digitalWrite(step_alignPin, LOW);
-       delayMicroseconds(1000);
+       delayMicroseconds(700);
        }
     
-    //turn(step_alignPin, 60);
+    
     break;
-  case 2:
-    digitalWrite(dir_alignPin, HIGH); // counterclockwise rotation
-    turn(step_alignPin, 60);
+   case 2:
+   
     digitalWrite(dir_alignPin, LOW); // clockwise rotation
-    turn(step_alignPin, 60);
+    for(int x = 0; x < 3333; x++)
+      {
+       digitalWrite(step_alignPin, HIGH);
+       delayMicroseconds(400);
+       digitalWrite(step_alignPin, LOW);
+       delayMicroseconds(400);
+       }
+    
+    
+    digitalWrite(dirPin, HIGH); //conveyor direction
+    for(int x = 0; x < 20000; x++)
+      {
+       digitalWrite(stepPin, HIGH);
+       delayMicroseconds(400);
+       digitalWrite(stepPin, LOW);
+       delayMicroseconds(400);
+       }
+
+
+    
+    digitalWrite(dir_alignPin, HIGH); // counterclockwise rotation
+    for(int x = 0; x < 3333; x++)
+      {
+       digitalWrite(step_alignPin, HIGH);
+       delayMicroseconds(400);
+       digitalWrite(step_alignPin, LOW);
+       delayMicroseconds(400);
+       }
     break;
     
-  case 0:
+  case 3:
+    digitalWrite(dirPin, LOW); //conveyor direction
+   
+    for(int x = 0; x < 20000; x++) // 40 seconds for a full rotation at 1ms delay
+      {
+       digitalWrite(stepPin, HIGH);
+       delayMicroseconds(400);
+       digitalWrite(stepPin, LOW);
+       delayMicroseconds(400);
+       }
     break;
 
   default:
@@ -68,19 +108,4 @@ void setup()
 
 }
 
-}
-
-void turn(int pin, int angle) 
-{
-  int stepsPerRevolution = 20000;
-  //int stepPin = pin;
-  int iterations = floor(angle*stepsPerRevolution/360);
-
-for(int x = 0; x < iterations; x++)
-  {
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(1000);
-  }
 }
